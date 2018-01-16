ae6200178421ae1bb7c1442240e112bc205b2fa6
David Hess <davehess@gmail.com>
2018-01-15 19:28:24 -0500

    First commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent implements OnInit {
+  public isCollapsed = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
